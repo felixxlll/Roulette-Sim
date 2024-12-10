7a5c81c13157df51b7246a40963f1e0d62cd6ea6
Felix Larsson <felix.fl2001@gmail.com>
2024-12-10 17:59:51 +0100

    Started html, added jquery and started js script

---
+++ scripts/roulette.js
@@ -0,0 +1,24 @@
+function SpinBarrel(player) {
+    
+};
+
+function Shoot(player) {
+
+};
+
+const player = {
+    name: "John",
+    shots: 0
+};
+
+function CreatePlayer(name) {
+    this.name = name
+    this.shots = 1 + Math.floor(Math.random() * 6)
+}
+
+$('#')
+
+list_of_names = [
+
+]
+
