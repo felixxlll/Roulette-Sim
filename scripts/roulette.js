4106635751f0b0711fefdb5d14c3893355ee8e9d
Felix Larsson <felix.fl2001@gmail.com>
2024-12-10 22:32:28 +0100

    Added some comments to js

--- scripts/roulette.js
+++ scripts/roulette.js
@@ -1,13 +1,21 @@
+// Prevents form submission from reloading the page
 $('form').submit(function (e) {
     e.preventDefault();
 });
 
-function SpinBarrel(player) {
+// Reloads a players gun
+function Reload(player) {
     
 };
 
+// Reloads all guns
+function ReloadAll() {
+
+}
+
+// 
 function Shoot(player) {
-    // if player.shots == 0, youre dead
+    // if player.shots == 0, you're dead
 };
 
 const player = {
@@ -15,11 +23,16 @@ const player = {
     shots: 0
 };
 
-let list_of_names = []
+// IMPORTANT!
+// All changes made to this list should instead be made to list_of_players_proxy
+// This list is only managed by the proxy. Maybe this should be the other way around...(?)
+let list_of_players = []
 
-let list_of_names_proxy = new Proxy(list_of_names, {
+// Creates a proxy to check for changes in the list_of_players variable
+// The main purpose for this is to update the website whenever you add or remove players.
+let list_of_players_proxy = new Proxy(list_of_players, {
     set: function (target, property, value) {
-        onListChange(); // Trigger whenever the list changes
+        onListChange(); // Triggers whenever the list changes
         target[property] = value; // Perform the default array operation
         return true; // Indicate success
     }
@@ -30,16 +43,18 @@ function onListChange() {
     console.log("List updated")
 }
 
+// Adds player whenever form is submitted, or rather whenever the button is clicked
 $('#name-submit').click(function CreatePlayer(){
     let player = {
         name: $('#name').val(),
         shots: 1 + Math.floor(Math.random() * 6)
     }
-    list_of_names_proxy.push(player)
+    list_of_players_proxy.push(player)
 })
 
+// Removes all players by clearing the list
 $('#name-clear').click(function ClearNames(){
-    list_of_names_proxy.length = 0;
+    list_of_players_proxy.length = 0;
 })
 
 
