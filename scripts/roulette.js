21931dc049cf33209f159fadd00612995f6b8502
Felix Larsson <felix.fl2001@gmail.com>
2024-12-16 03:03:49 +0100

    oops

--- scripts/roulette.js
+++ scripts/roulette.js
@@ -22,7 +22,7 @@ function Reload(playerName) {
         return;
     }
 
-    $(chambers).each(function(t){
+    $(chambers).each(function(){
         $(this).removeClass('fired')
         $(this).removeClass('lethal')
     })
